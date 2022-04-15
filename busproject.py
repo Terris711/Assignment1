ffb502c06dbda253aba48a7496294519a215b799
Thi Van Anh Duong <90023112@study.curtincollege.edu.au>
2022-04-14 21:55:31 -0700

    Fixed2

--- busproject.py
+++ busproject.py
@@ -1,25 +1,7 @@
 # Name: Thi Van Anh Duong
 # Student ID: 90023112
 #
-# Assuming this is the code (python language) for the bus management system 
+# busproject.py: Assuming this is the code (python language) for the bus management system 
 #
 
-#Create Database code here
-
-
-
-
-#Create Sign in and Log in interface
-
-
-
-#Create functional features
-
-
-
-#Create ticket features
-
-
-
-#Create Journey features
 
