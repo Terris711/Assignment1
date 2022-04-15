0ba4342f0f0c664d29e0728fe025dc35edf73dbe
Thi Van Anh Duong <90023112@study.curtincollege.edu.au>
2022-04-14 21:31:25 -0700

    Second Commit

--- busproject.py
+++ busproject.py
@@ -3,3 +3,14 @@
 #
 # Assuming this is the code (python language) for the bus management system 
 #
+
+#Create Database code here
+
+#Create Sign in and Log in interface
+
+#Create functional features
+
+#Create ticket features
+
+#Create Journey features
+
